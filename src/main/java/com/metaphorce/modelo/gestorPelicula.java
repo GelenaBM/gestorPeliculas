5559c6211706175d984ef01feb6e0796dcc88751
GelenaBM <67291901+GelenaBM@users.noreply.github.com>
2023-10-16 20:58:43 -0600

    Update gestorPelicula.java

--- src/main/java/com/metaphorce/modelo/gestorPelicula.java
+++ src/main/java/com/metaphorce/modelo/gestorPelicula.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class gestorPelicula extends pelicula {
-    //Lista
+    //Crea la lista inicial de películas
 
     List<List<String>> list = new ArrayList<List<String>>();
 
